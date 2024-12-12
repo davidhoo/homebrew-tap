f61b7b2a12581888b4fbf15c974d5d532ced9018
David Hu <hubo3@staff.weibo.com>
2024-12-12 16:45:47 +0800

    jsonpath 1.0.1 (new formula)
    Add jsonpath formula - an RFC 9535 compliant JSONPath implementation in Go

--- Formula/jsonpath.rb
+++ Formula/jsonpath.rb
@@ -1,25 +1,25 @@
 class Jsonpath < Formula
   desc "RFC 9535 compliant JSONPath implementation in Go"
   homepage "https://github.com/davidhoo/jsonpath"
-  version "1.0.0"
+  version "1.0.1"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.0/jp-darwin-arm64.tar.gz"
-      sha256 "05ca13767b6d9aad6f94378cf35d4fb1da9c48d3d18d9a407f6fed7bdb3aa4e4"
+      url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.1/jp-darwin-arm64.tar.gz"
+      sha256 "53bae8aeb8a7c32f983ba9ddb794eb4ad57d3c35d980e617c36f31fb3eff9e6b"
     else
-      url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.0/jp-darwin-amd64.tar.gz"
-      sha256 "418c146536bfe2f662832434662f8fa2303b9de768cadc27c8e11ec145c3a929"
+      url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.1/jp-darwin-amd64.tar.gz"
+      sha256 "6f2830389413ab1497d6bd74df37577527854adca5e23ec0ad22f1da64ac9377"
     end
   end
   
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.0/jp-linux-arm64.tar.gz"
-      sha256 "c7d96c3f191f1b2629442ec85ef2396eeecb0597df583ad9953d7d8c4a78d5ee"
+      url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.1/jp-linux-arm64.tar.gz"
+      sha256 "f69ac99d468737a5bd09d067e1a1c30ffe0c60adf706dbf75d9c3262a2833b50"
     else
-      url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.0/jp-linux-amd64.tar.gz"
-      sha256 "4aa7c75d962d4a1c20672aca3ea7755372770df6148bf927e0ff98d735399135"
+      url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.1/jp-linux-amd64.tar.gz"
+      sha256 "baddd0334c7a8589a4f5997d31aa71b5943f2e0eb71e80f2aa05d1c512bf060e"
     end
   end
 
