36e151187b8e0f867a6a80a2614bf0689a544956
David Hu <hubo3@staff.weibo.com>
2024-12-16 16:09:24 +0800

    feat: update jsonpath to v1.0.3 with new checksums

--- Formula/jsonpath.rb
+++ Formula/jsonpath.rb
@@ -6,20 +6,20 @@ class Jsonpath < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.3/jp-darwin-arm64.tar.gz"
-      sha256 "53bae8aeb8a7c32f983ba9ddb794eb4ad57d3c35d980e617c36f31fb3eff9e6b"
+      sha256 "c6881a8cea74e3759de052ee97a9ebf588c3acb6fddaa0215e04ce958e9322ed"
     else
       url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.3/jp-darwin-amd64.tar.gz"
-      sha256 "6f2830389413ab1497d6bd74df37577527854adca5e23ec0ad22f1da64ac9377"
+      sha256 "a27021d3fa117f6d3b196486ad2df00a6afb6196a3ab77da9b0bc61248573926"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.3/jp-linux-arm64.tar.gz"
-      sha256 "f69ac99d468737a5bd09d067e1a1c30ffe0c60adf706dbf75d9c3262a2833b50"
+      sha256 "47016d5ba5d439d02b31da1ae5191112b54ac5f4769d93990d349caa74d37ce7"
     else
       url "https://github.com/davidhoo/jsonpath/releases/download/v1.0.3/jp-linux-amd64.tar.gz"
-      sha256 "baddd0334c7a8589a4f5997d31aa71b5943f2e0eb71e80f2aa05d1c512bf060e"
+      sha256 "5dad5c444999dfac65749bd7a9653547bf859e5b8269a207c8d140f31a565bea"
     end
   end
 
