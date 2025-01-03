10269a558c4765350e8c3ce2b7c5bd5dadec909c
David Hu <hubo3@staff.weibo.com>
2025-01-03 15:43:37 +0800

    fix(formula): simplify jsonpath installation as binary is now named jp

--- Formula/jsonpath.rb
+++ Formula/jsonpath.rb
@@ -6,20 +6,20 @@ class Jsonpath < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/davidhoo/jsonpath/releases/download/v2.0.1/jp-darwin-arm64.tar.gz"
-      sha256 "23ff9552f815e5e99c7399b107a836e38e62d51d604552298dd1f4939cc693eb"
+      sha256 "f61f2c6db3a650b2ffa1f501727b0b74641637687c2cf841217ad710401217d1"
     else
       url "https://github.com/davidhoo/jsonpath/releases/download/v2.0.1/jp-darwin-amd64.tar.gz"
-      sha256 "295484f38043a27dbbf9c39c6d282e2680ef0eae6dd248820f1523857f3a118a"
+      sha256 "3bb38be712d7687e1bde8b3661c425184d0a6217c72647f252c5552f0770a965"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/davidhoo/jsonpath/releases/download/v2.0.1/jp-linux-arm64.tar.gz"
-      sha256 "98669d1164db897361621fb4ac99ad158a16b4c73cd8224c54379a29241a1192"
+      sha256 "41926cfb0d19dcac79bc1493cb28e845eabe08bb68eede541727fad4e890b53f"
     else
       url "https://github.com/davidhoo/jsonpath/releases/download/v2.0.1/jp-linux-amd64.tar.gz"
-      sha256 "c89b33b7c6c5f2b4833403d32a5c1d69b94345db748f9b52d874903ef3d78831"
+      sha256 "42d655de37b660e7775f844b922bb75df6811f50239d5e3c28f0122d820a8108"
     end
   end
 
