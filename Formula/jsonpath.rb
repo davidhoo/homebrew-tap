7216f6da0f347b78a685f5ea92e60d7013582a8c
David Hu <coolbor@gmail.com>
2026-05-07 19:36:36 +0800

    feat: update jsonpath to v3.0.0
    - 100% RFC 9535 compliance (703/703 tests)
    - Breaking changes: Query() returns NodeList
    - New functions: value(), occurrences()
    - I-Regexp support, Normalized Path
    - CLI --path flag

--- Formula/jsonpath.rb
+++ Formula/jsonpath.rb
@@ -1,25 +1,28 @@
 class Jsonpath < Formula
   desc "RFC 9535 compliant JSONPath processor with beautiful colored output"
   homepage "https://github.com/davidhoo/jsonpath"
-  version "2.0.1"
+  version "3.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/davidhoo/jsonpath/releases/download/v2.0.1/jp-darwin-arm64.tar.gz"
-      sha256 "f61f2c6db3a650b2ffa1f501727b0b74641637687c2cf841217ad710401217d1"
+      url "https://github.com/davidhoo/jsonpath/releases/download/v3.0.0/jsonpath_Darwin_arm64.tar.gz"
+      sha256 "5dae0d97705c3b4e47b38156a1c4a424484cb5a4324e165b6b1c1d9fa3863dd0"
     else
-      url "https://github.com/davidhoo/jsonpath/releases/download/v2.0.1/jp-darwin-amd64.tar.gz"
-      sha256 "3bb38be712d7687e1bde8b3661c425184d0a6217c72647f252c5552f0770a965"
+      url "https://github.com/davidhoo/jsonpath/releases/download/v3.0.0/jsonpath_Darwin_x86_64.tar.gz"
+      sha256 "892a9261030d0b7a3c815bb5ced7e24b89a7afa36e8a1091afae81d7b1771598"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/davidhoo/jsonpath/releases/download/v2.0.1/jp-linux-arm64.tar.gz"
-      sha256 "41926cfb0d19dcac79bc1493cb28e845eabe08bb68eede541727fad4e890b53f"
+      url "https://github.com/davidhoo/jsonpath/releases/download/v3.0.0/jsonpath_Linux_arm64.tar.gz"
+      sha256 "7270c2e247f868607901e019af9c4cb86200b02c0761e4519affcc754d427788"
+    elsif Hardware::CPU.is_32_bit?
+      url "https://github.com/davidhoo/jsonpath/releases/download/v3.0.0/jsonpath_Linux_i386.tar.gz"
+      sha256 "0c9b3d8b2fdd3980ee04b4cf5382f7db7afede40ecda439790731fc27664325e"
     else
-      url "https://github.com/davidhoo/jsonpath/releases/download/v2.0.1/jp-linux-amd64.tar.gz"
-      sha256 "42d655de37b660e7775f844b922bb75df6811f50239d5e3c28f0122d820a8108"
+      url "https://github.com/davidhoo/jsonpath/releases/download/v3.0.0/jsonpath_Linux_x86_64.tar.gz"
+      sha256 "14637f2fa5aace1686432a0934b75391d3e13ad99fd76915fa99017046181504"
     end
   end
 
@@ -28,6 +31,7 @@ class Jsonpath < Formula
   end
 
   test do
-    assert_equal "\"jp\"", shell_output("#{bin}/jp -p '$.name' <<< '{\"name\":\"jp\"}'").strip
+    assert_match "v3.0.0", shell_output("#{bin}/jp -v")
+    assert_equal '"test"', shell_output("#{bin}/jp -p '$.name' <<< '{\"name\":\"test\"}'").strip
   end
-end 
+end
